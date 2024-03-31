7fbf9f571d379805ad622ea1428e47ca666fb6ad
ghram <ghramalhajyalhajy@gmail.com>
2024-03-31 13:37:21 +0300

    -martadaleh

--- language-center-app/src/main/java/Geeks/languagecenterapp/LanguageCenterAppApplication.java
+++ language-center-app/src/main/java/Geeks/languagecenterapp/LanguageCenterAppApplication.java
@@ -2,13 +2,3 @@ package Geeks.languagecenterapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class LanguageCenterAppApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(LanguageCenterAppApplication.class, args);
-	}
-	///ghram
-
-}
