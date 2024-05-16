a37bf13bf8fae96e7340b217649e937158e739ce
#YSR_MRZ <ysrmrz321@gmail.com>
2024-05-16 17:33:34 +0300

    Models & Repositories

---
+++ language-center-app/src/main/java/Geeks/languagecenterapp/Controller/UserController.java
@@ -0,0 +1,14 @@
+package Geeks.languagecenterapp.Controller;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.web.bind.annotation.RestController;
+@Getter
+@Setter
+@RestController
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserController {
+}
---
+++ language-center-app/src/main/java/Geeks/languagecenterapp/Service/UserService.java
@@ -0,0 +1,16 @@
+package Geeks.languagecenterapp.Service;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RestController;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Service
+public class UserService {
+}
